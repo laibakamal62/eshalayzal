4a50c2bc3103407bb9305e6ccf80c3e89d5d86a1
Laibakamal123455789 <168301675+Laibakamal123455789@users.noreply.github.com>
2025-08-22 16:18:25 +0500

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['images.pexels.com', 'localhost'],
+    domains: [
+      "images.pexels.com", 
+      "localhost", 
+      "eshal-ayzal.vercel.app" // ✅ add your admin domain
+    ],
   },
 };
 
